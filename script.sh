86903233b74c679a0db2ed015ef4b0e8017d1736
teresalazar13 <teresa.sal13@gmail.com>
2023-11-21 12:06:12 +0000

    Updates script.

--- script.sh
+++ script.sh
@@ -1,9 +1,3 @@
-#python3 main.py --fl Oracle --dataset CIFAR-GDrift --varying_disc 0.5  # local
-#python3 main.py --fl FedDrift --dataset CIFAR-GDrift --varying_disc 0.5 --thresholds 1.0  # cvd
-#python3 main.py --fl FairFedDrift --dataset CIFAR-GDrift --varying_disc 0.5 --thresholds 1.0 1.0  # arya
-
-python3 main.py --fl FedDrift --dataset CIFAR-GDrift --varying_disc 0.25 --thresholds 0.5  # local 2
-python3 main.py --fl FedDrift --dataset CIFAR-GDrift --varying_disc 0.25 --thresholds 1.0 1.0  # arya 2
-python3 main.py --fl FedAvg --dataset CIFAR-GDrift --varying_disc 0.75  # cvd 2
-
-python3 main.py --fl Oracle --dataset CIFAR-GDrift --varying_disc 0.75  # arya 3
+python3 main.py --fl FedDrift --dataset CIFAR-GDrift --varying_disc 0.25 --thresholds 0.5  # arya
+python3 main.py --fl FedDrift --dataset CIFAR-GDrift --varying_disc 0.75 --thresholds 1.0 # cvd-2
+python3 main.py --fl FairFedDrift --dataset CIFAR-GDrift --varying_disc 0.75 --thresholds 1.0 1.0 # cvd-4
