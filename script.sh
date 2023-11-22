1d3dfe9b6fe69f37af6825300305b104ebbb192d
teresalazar13 <teresa.sal13@gmail.com>
2023-11-22 01:02:06 +0000

    Adds results and scenarios.

--- script.sh
+++ script.sh
@@ -1,6 +1,39 @@
-python3 main.py --fl FedDrift --dataset CIFAR-GDrift --varying_disc 0.25 --thresholds 0.5  # arya
-python3 main.py --fl FairFedDrift --dataset CIFAR-GDrift --varying_disc 0.75 --thresholds 1.0 1.0 # cvd-4
-python3 main.py --fl FedDrift --dataset CIFAR-GDrift --varying_disc 0.75 --thresholds 1.0 # cvd-2
+#python3 main.py --scenario 2 --fl FedAvg --dataset MNIST-GDrift --varying_disc 0.1
+#python3 main.py --scenario 2 --fl Oracle --dataset MNIST-GDrift --varying_disc 0.1
+#python3 main.py --scenario 2 --fl FedAvg --dataset MNIST-GDrift --varying_disc 0.25
+#python3 main.py --scenario 2 --fl Oracle --dataset MNIST-GDrift --varying_disc 0.25
+#python3 main.py --scenario 2 --fl FedAvg --dataset MNIST-GDrift --varying_disc 0.5
+#python3 main.py --scenario 2 --fl Oracle --dataset MNIST-GDrift --varying_disc 0.5
 
-python3 main.py --fl FedDrift --dataset CIFAR-GDrift --varying_disc 0.1 --thresholds 0.1 # crai
+python3 main.py --scenario 2 --fl FedDrift --dataset MNIST-GDrift --varying_disc 0.1 --thresholds 0.1
+python3 main.py --scenario 2 --fl FedDrift --dataset MNIST-GDrift --varying_disc 0.1 --thresholds 0.25
+python3 main.py --scenario 2 --fl FedDrift --dataset MNIST-GDrift --varying_disc 0.1 --thresholds 0.5
+python3 main.py --scenario 2 --fl FedDrift --dataset MNIST-GDrift --varying_disc 0.1 --thresholds 0.75
+python3 main.py --scenario 2 --fl FedDrift --dataset MNIST-GDrift --varying_disc 0.1 --thresholds 1.0
+python3 main.py --scenario 2 --fl FairFedDrift --dataset MNIST-GDrift --varying_disc 0.1 --thresholds 0.1 0.1
+python3 main.py --scenario 2 --fl FairFedDrift --dataset MNIST-GDrift --varying_disc 0.1 --thresholds 0.25 0.25
+python3 main.py --scenario 2 --fl FairFedDrift --dataset MNIST-GDrift --varying_disc 0.1 --thresholds 0.5 0.5
+python3 main.py --scenario 2 --fl FairFedDrift --dataset MNIST-GDrift --varying_disc 0.1 --thresholds 0.75 0.75
+python3 main.py --scenario 2 --fl FairFedDrift --dataset MNIST-GDrift --varying_disc 0.1 --thresholds 1.0 1.0
 
+python3 main.py --scenario 2 --fl FedDrift --dataset MNIST-GDrift --varying_disc 0.25 --thresholds 0.1
+python3 main.py --scenario 2 --fl FedDrift --dataset MNIST-GDrift --varying_disc 0.25 --thresholds 0.25
+python3 main.py --scenario 2 --fl FedDrift --dataset MNIST-GDrift --varying_disc 0.25 --thresholds 0.5
+python3 main.py --scenario 2 --fl FedDrift --dataset MNIST-GDrift --varying_disc 0.25 --thresholds 0.75
+python3 main.py --scenario 2 --fl FedDrift --dataset MNIST-GDrift --varying_disc 0.25 --thresholds 1.0
+python3 main.py --scenario 2 --fl FairFedDrift --dataset MNIST-GDrift --varying_disc 0.25 --thresholds 0.1 0.1
+python3 main.py --scenario 2 --fl FairFedDrift --dataset MNIST-GDrift --varying_disc 0.25 --thresholds 0.25 0.25
+python3 main.py --scenario 2 --fl FairFedDrift --dataset MNIST-GDrift --varying_disc 0.25 --thresholds 0.5 0.5
+python3 main.py --scenario 2 --fl FairFedDrift --dataset MNIST-GDrift --varying_disc 0.25 --thresholds 0.75 0.75
+python3 main.py --scenario 2 --fl FairFedDrift --dataset MNIST-GDrift --varying_disc 0.25 --thresholds 1.0 1.0
+
+python3 main.py --scenario 2 --fl FedDrift --dataset MNIST-GDrift --varying_disc 0.5 --thresholds 0.1
+python3 main.py --scenario 2 --fl FedDrift --dataset MNIST-GDrift --varying_disc 0.5 --thresholds 0.25
+python3 main.py --scenario 2 --fl FedDrift --dataset MNIST-GDrift --varying_disc 0.5 --thresholds 0.5
+python3 main.py --scenario 2 --fl FedDrift --dataset MNIST-GDrift --varying_disc 0.5 --thresholds 0.75
+python3 main.py --scenario 2 --fl FedDrift --dataset MNIST-GDrift --varying_disc 0.5 --thresholds 1.0
+python3 main.py --scenario 2 --fl FairFedDrift --dataset MNIST-GDrift --varying_disc 0.5 --thresholds 0.1 0.1
+python3 main.py --scenario 2 --fl FairFedDrift --dataset MNIST-GDrift --varying_disc 0.5 --thresholds 0.25 0.25
+python3 main.py --scenario 2 --fl FairFedDrift --dataset MNIST-GDrift --varying_disc 0.5 --thresholds 0.5 0.5
+python3 main.py --scenario 2 --fl FairFedDrift --dataset MNIST-GDrift --varying_disc 0.5 --thresholds 0.75 0.75
+python3 main.py --scenario 2 --fl FairFedDrift --dataset MNIST-GDrift --varying_disc 0.5 --thresholds 1.0 1.0
---
+++ script_old.sh
@@ -0,0 +1,39 @@
+python3 main.py --scenario 2 --fl FedAvg --dataset FashionMNIST-GDrift --varying_disc 0.1
+python3 main.py --scenario 2 --fl Oracle --dataset FashionMNIST-GDrift --varying_disc 0.1
+python3 main.py --scenario 2 --fl FedAvg --dataset FashionMNIST-GDrift --varying_disc 0.25
+python3 main.py --scenario 2 --fl Oracle --dataset FashionMNIST-GDrift --varying_disc 0.25
+python3 main.py --scenario 2 --fl FedAvg --dataset FashionMNIST-GDrift --varying_disc 0.5
+python3 main.py --scenario 2 --fl Oracle --dataset FashionMNIST-GDrift --varying_disc 0.5
+
+python3 main.py --scenario 2 --fl FedDrift --dataset FashionMNIST-GDrift --varying_disc 0.1 --thresholds 0.1
+python3 main.py --scenario 2 --fl FedDrift --dataset FashionMNIST-GDrift --varying_disc 0.1 --thresholds 0.25
+python3 main.py --scenario 2 --fl FedDrift --dataset FashionMNIST-GDrift --varying_disc 0.1 --thresholds 0.5
+python3 main.py --scenario 2 --fl FedDrift --dataset FashionMNIST-GDrift --varying_disc 0.1 --thresholds 0.75
+python3 main.py --scenario 2 --fl FedDrift --dataset FashionMNIST-GDrift --varying_disc 0.1 --thresholds 1.0
+python3 main.py --scenario 2 --fl FairFedDrift --dataset FashionMNIST-GDrift --varying_disc 0.1 --thresholds 0.1 0.1
+python3 main.py --scenario 2 --fl FairFedDrift --dataset FashionMNIST-GDrift --varying_disc 0.1 --thresholds 0.25 0.25
+python3 main.py --scenario 2 --fl FairFedDrift --dataset FashionMNIST-GDrift --varying_disc 0.1 --thresholds 0.5 0.5
+python3 main.py --scenario 2 --fl FairFedDrift --dataset FashionMNIST-GDrift --varying_disc 0.1 --thresholds 0.75 0.75
+python3 main.py --scenario 2 --fl FairFedDrift --dataset FashionMNIST-GDrift --varying_disc 0.1 --thresholds 1.0 1.0
+
+python3 main.py --scenario 2 --fl FedDrift --dataset FashionMNIST-GDrift --varying_disc 0.25 --thresholds 0.1
+python3 main.py --scenario 2 --fl FedDrift --dataset FashionMNIST-GDrift --varying_disc 0.25 --thresholds 0.25
+python3 main.py --scenario 2 --fl FedDrift --dataset FashionMNIST-GDrift --varying_disc 0.25 --thresholds 0.5
+python3 main.py --scenario 2 --fl FedDrift --dataset FashionMNIST-GDrift --varying_disc 0.25 --thresholds 0.75
+python3 main.py --scenario 2 --fl FedDrift --dataset FashionMNIST-GDrift --varying_disc 0.25 --thresholds 1.0
+python3 main.py --scenario 2 --fl FairFedDrift --dataset FashionMNIST-GDrift --varying_disc 0.25 --thresholds 0.1 0.1
+python3 main.py --scenario 2 --fl FairFedDrift --dataset FashionMNIST-GDrift --varying_disc 0.25 --thresholds 0.25 0.25
+python3 main.py --scenario 2 --fl FairFedDrift --dataset FashionMNIST-GDrift --varying_disc 0.25 --thresholds 0.5 0.5
+python3 main.py --scenario 2 --fl FairFedDrift --dataset FashionMNIST-GDrift --varying_disc 0.25 --thresholds 0.75 0.75
+python3 main.py --scenario 2 --fl FairFedDrift --dataset FashionMNIST-GDrift --varying_disc 0.25 --thresholds 1.0 1.0
+
+python3 main.py --scenario 2 --fl FedDrift --dataset FashionMNIST-GDrift --varying_disc 0.5 --thresholds 0.1
+python3 main.py --scenario 2 --fl FedDrift --dataset FashionMNIST-GDrift --varying_disc 0.5 --thresholds 0.25
+python3 main.py --scenario 2 --fl FedDrift --dataset FashionMNIST-GDrift --varying_disc 0.5 --thresholds 0.5
+python3 main.py --scenario 2 --fl FedDrift --dataset FashionMNIST-GDrift --varying_disc 0.5 --thresholds 0.75
+python3 main.py --scenario 2 --fl FedDrift --dataset FashionMNIST-GDrift --varying_disc 0.5 --thresholds 1.0
+python3 main.py --scenario 2 --fl FairFedDrift --dataset FashionMNIST-GDrift --varying_disc 0.5 --thresholds 0.1 0.1
+python3 main.py --scenario 2 --fl FairFedDrift --dataset FashionMNIST-GDrift --varying_disc 0.5 --thresholds 0.25 0.25
+python3 main.py --scenario 2 --fl FairFedDrift --dataset FashionMNIST-GDrift --varying_disc 0.5 --thresholds 0.5 0.5
+python3 main.py --scenario 2 --fl FairFedDrift --dataset FashionMNIST-GDrift --varying_disc 0.5 --thresholds 0.75 0.75
+python3 main.py --scenario 2 --fl FairFedDrift --dataset FashionMNIST-GDrift --varying_disc 0.5 --thresholds 1.0 1.0
