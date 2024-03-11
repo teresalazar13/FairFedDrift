312614be7033aab4cc06e3909b47988424ee2a73
teresalazar13 <teresa.sal13@gmail.com>
2024-03-11 19:37:11 +0000

    Prepares to run automatically.

--- script.sh
+++ script.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
-DATASET="$1"
-ALPHA="$2"
-CSV="$3"
+#scenarios=(1 2 3)
+#thresholds=(0.05 0.1 0.25 0.5 0.75 1.0 1.5)
+scenarios=(1)
+thresholds=(0.05 0.1)
+counter=1
 
-# Read the CSV file line by line
-while IFS=, read -r screen_command; do
-    # Extract screen name and command from each line
-    screen_name=$(echo "$screen_command" | cut -d',' -f1)
-    command_to_run=$(echo "$screen_command" | cut -d',' -f2-)
+for scenario in "${scenarios[@]}"; do
+    screen -dms "$counter"
+    screen -x "$counter" CUDA_VISIBLE_DEVICES="$core" python3 main.py --scenario "$scenario" --fl FedAvg --dataset DATASET --varying_disc 0.05
+    ((counter++))
 
-    # Replace DATASET and ALPHA in the command
-    command_to_run="${command_to_run//DATASET/$DATASET}"
-    command_to_run="${command_to_run//ALPHA/$ALPHA}"
+    core=$(echo "scale=0; $counter / (50/3)" | bc)
+    screen -dms "$counter"
+    screen -x "$counter" CUDA_VISIBLE_DEVICES="$core" python3 main.py --scenario "$scenario" --fl Oracle --dataset DATASET --varying_disc 0.05
+    ((counter++))
 
-    # Create a screen session with the screen name and run the command
-    screen -dmS "$screen_name" bash -c "$command_to_run"
+    for threshold in "${thresholds[@]}"; do
+        core=$(echo "scale=0; $counter / (50/3)" | bc)
 
-done < CSV
+        core=$(echo "scale=0; $counter / (50/3)" | bc)
+        screen -dms "$counter"
+        screen -x "$counter" CUDA_VISIBLE_DEVICES="$core" python3 main.py --scenario "$scenario" --fl FedDrift --dataset DATASET --thresholds "$threshold" --varying_disc 0.05
+        ((counter++))
+
+        core=$(echo "scale=0; $counter / (50/3)" | bc)
+        screen -dms "$counter"
+        screen -dms "$counter" CUDA_VISIBLE_DEVICES="$core" python3 main.py --scenario "$scenario" --fl FairFedDrift --dataset DATASET --thresholds "$threshold" --varying_disc 0.05
+        ((counter++))
+    done
+done
