9e91d2e6cb6a45abbb251eb049b6689f507c64de
teresalazar13 <teresa.sal13@gmail.com>
2024-10-18 14:47:09 +0100

    Removes unnecessary files.

--- datasets/DatasetFactory.py
+++ datasets/DatasetFactory.py
@@ -1,11 +1,10 @@
-from datasets.image.CelebA_GDrift.CelebA_GDrift import CelebA_GDrift
 from datasets.tabular.Adult_GDrift.Adult_GDrift import Adult_GDrift
 from datasets.image.MNIST_GDrift.MNIST_GDrift import MNIST_GDrift
 from datasets.image.FashionMNIST_GDrift.FashionMNIST_GDrift import FashionMNIST_GDrift
 
 
 def get_datasets():
-    return [MNIST_GDrift(), FashionMNIST_GDrift(), CelebA_GDrift(), Adult_GDrift()]
+    return [MNIST_GDrift(), FashionMNIST_GDrift(), Adult_GDrift()]
 
 
 def get_dataset_by_name(name):
