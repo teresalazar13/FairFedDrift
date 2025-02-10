ba9594a066b8499798205f47300c14195bc69e09
teresalazar13 <teresa.sal13@gmail.com>
2025-02-10 16:05:42 +0000

    Changes parameters.

--- federated/model.py
+++ federated/model.py
@@ -34,6 +34,7 @@ class NN_model:
                 self.model.add(tf.keras.layers.BatchNormalization())
                 self.model.add(tf.keras.layers.Dense(100, activation='softmax'))
                 dummy_input = tf.random.normal([1] + dataset.input_shape)  # batch size of 1, correct input shape
+                dummy_input = tf.random.normal([1] + list(dataset.input_shape))  # batch size of 1, correct input shape
                 self.model.build(input_shape=dummy_input.shape)
 
             else:  # MNIST and FEMNIST
