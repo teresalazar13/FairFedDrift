3f145e9bbc7ca0f6722b09741d6c7779d2899be1
teresalazar13 <teresa.sal13@gmail.com>
2025-02-10 16:06:43 +0000

    Changes parameters.

--- federated/model.py
+++ federated/model.py
@@ -33,7 +33,6 @@ class NN_model:
                 self.model.add(tf.keras.layers.Dropout(.25))
                 self.model.add(tf.keras.layers.BatchNormalization())
                 self.model.add(tf.keras.layers.Dense(100, activation='softmax'))
-                dummy_input = tf.random.normal([1] + dataset.input_shape)  # batch size of 1, correct input shape
                 dummy_input = tf.random.normal([1] + list(dataset.input_shape))  # batch size of 1, correct input shape
                 self.model.build(input_shape=dummy_input.shape)
 
