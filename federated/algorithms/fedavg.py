ee23a107429dd5567af632cd5453f1ef72b2a62c
teresalazar13 <teresa.sal13@gmail.com>
2025-02-15 19:26:40 +0000

    Changes parameters.

--- federated/algorithms/fedavg.py
+++ federated/algorithms/fedavg.py
@@ -6,6 +6,8 @@ from tensorflow.keras import backend as K
 import logging
 import time
 import gc
+import tensorflow as tf
+import psutil
 
 
 class FedAvg(Algorithm):
@@ -17,6 +19,11 @@ class FedAvg(Algorithm):
         super().__init__(name, color, marker)
 
     def perform_fl(self, seed, clients_data, dataset):
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        if gpus:
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+
         global_model = NN_model(dataset, seed)
         clients_metrics = [get_metrics(dataset.is_binary_target) for _ in range(dataset.n_clients)]
         # Train with data from first timestep
@@ -42,11 +49,13 @@ def train_and_average(global_model, dataset, clients_data, timestep, seed):
         global_weights_summed = []
 
         for client in range(dataset.n_clients):
+            print_memory_usage("Before Training")
+            print_gpu_memory()
+
             x, y, s, _ = clients_data[timestep][client]
-            global_weights = global_model.get_weights()
             local_model = NN_model(dataset, seed)
             local_model.compile(dataset)
-            local_model.set_weights(global_weights)
+            local_model.set_weights(global_model.get_weights())
             local_model.learn(x, y)
             local_count = len(x)
             local_weights = local_model.get_weights()
@@ -57,10 +66,15 @@ def train_and_average(global_model, dataset, clients_data, timestep, seed):
             total_count += local_count
             logging.info("Trained model timestep {} cround {} client {}".format(timestep, cround, client))
 
-            K.clear_session()
-            del local_model, x, y, s, local_weights
+            print_memory_usage("After Training")
+            print_gpu_memory()
+
+            del local_model, x, y, s, _, local_weights  # TODO _
             K.clear_session()
             gc.collect()
+
+            print_memory_usage("After Cleanup")
+            print_gpu_memory()
 
         new_global_weights = divide_weights(global_weights_summed, total_count)
         global_model.set_weights(new_global_weights)
@@ -72,3 +86,17 @@ def train_and_average(global_model, dataset, clients_data, timestep, seed):
         gc.collect()
 
     return global_model
+
+
+def print_gpu_memory():
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        for gpu in gpus:
+            details = tf.config.experimental.get_memory_info(gpu.name)
+            print(f"GPU Memory (Used): {details['current'] / 1024**2:.2f} MB")
+
+
+def print_memory_usage(label):
+    process = psutil.Process()
+    mem = process.memory_info().rss / (1024 * 1024)  # Convert bytes to MB
+    print(f"[{label}] Memory Usage: {mem:.2f} MB")
