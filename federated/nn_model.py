a5bb4875b7a1da0cd68cf0c7337e8af58cc40007
teresalazar13 <teresa.sal13@gmail.com>
2025-02-21 15:14:25 +0000

    Adds CIFAR-100 and Resnet.

--- federated/nn_model.py
+++ federated/nn_model.py
@@ -4,7 +4,7 @@ from federated.nn_tf import NNTF
 
 def NN_model(dataset, seed):
     if dataset.is_pt:
-        #return NNPT(dataset, seed)
-        return NNTF_2(dataset, seed)
+        return NNPT(dataset, seed)
+        #return NNTF_2(dataset, seed)
     else:
         return NNTF(dataset, seed)
