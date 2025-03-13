aaa46cd4f3a98c946d9617acc46e4e35b3c0901e
teresalazar13 <teresa.sal13@gmail.com>
2025-03-13 13:52:02 +0000

    Updates table with 1 work and 2 datasets.

--- federated/algorithms/oracle.py
+++ federated/algorithms/oracle.py
@@ -17,40 +17,47 @@ class Oracle(Algorithm):
 
     def perform_fl(self, seed, clients_data, dataset):
         clients_metrics = [get_metrics(dataset.is_binary_target) for _ in range(dataset.n_clients)]
-        global_models, clients_identities = setup(seed, dataset)
+        global_models, clients_identities, gm_has_been_trained = setup(seed, dataset)
         # Train with data from first timestep
-        global_models = train_and_average(global_models, dataset, clients_data, 0, seed)
+        global_models, gm_has_been_trained = train_and_average(global_models, dataset, clients_data, 0, gm_has_been_trained, seed)
 
         for timestep in range(1, dataset.n_timesteps):
             for client_id in range(dataset.n_clients):
                 drift_id = dataset.drift_ids[timestep - 1][client_id]
                 clients_identities[client_id].append(Identity(drift_id, drift_id))
             test_models(global_models, clients_data, clients_metrics, dataset, timestep, clients_identities)
-            global_models = train_and_average(global_models, dataset, clients_data, timestep, seed)
+            global_models, gm_has_been_trained = train_and_average(global_models, dataset, clients_data, timestep, gm_has_been_trained, seed)
 
         return clients_metrics, clients_identities
 
 
 def setup(seed, dataset):
     global_models = []
+    gm_has_been_trained = []
+
     for i in range(dataset.n_drifts):
-        global_models.append(NN_model(dataset, seed))
+        global_models.append(None)
+        gm_has_been_trained.append(False)
+
+    global_models[0] = NN_model(dataset, seed)
+    gm_has_been_trained[0] = True
     clients_identities = [[] for _ in range(dataset.n_clients)]
 
-    return global_models, clients_identities
+    return global_models, clients_identities, gm_has_been_trained
 
 
-def train_and_average(global_models, dataset, clients_data, timestep, seed):
+def train_and_average(global_models, dataset, clients_data, timestep, gm_has_been_trained, seed):
     for cround in range(dataset.n_rounds):
         local_weights_list = [[] for _ in range(len(global_models))]
         local_scales_list = [[] for _ in range(len(global_models))]
         for client_id in range(dataset.n_clients):
-            # 1 - Get client data of each global model of all timesteps until timestep (inclusive)
+            # Get client data of each global model of all timesteps until timestep (inclusive)
             x_shape = list(clients_data[0][client_id][0].shape)
             y_shape = list(clients_data[0][client_id][1].shape)
             x_shape[0] = 0
             y_shape[0] = 0
-            x = [np.empty(x_shape, dtype=np.float32) for _ in range(len(global_models))]  # create x, y for each drift (for each global model)
+            # create x, y for each drift (for each global model id)
+            x = [np.empty(x_shape, dtype=np.float32) for _ in range(len(global_models))]
             y = [np.empty(y_shape, dtype=np.float32) for _ in range(len(global_models))]
             logging.info("Getting data of client {} until timestep {} (inclusive)".format(client_id, timestep))
             for t_line in range(timestep + 1):
@@ -62,7 +69,13 @@ def train_and_average(global_models, dataset, clients_data, timestep, seed):
 
             for gm_id, (xx, yy) in enumerate(zip(x, y)):  # xx is data from a global model of client client_id
                 if len(xx) > 0:
-                    global_weights = global_models[gm_id].get_weights()
+                    if gm_has_been_trained[gm_id]:
+                        global_weights = global_models[gm_id].get_weights()
+                    else:
+                        global_models[gm_id] = NN_model(dataset, seed)
+                        global_models[gm_id].set_weights(global_models[0].get_weights())
+                        gm_has_been_trained[gm_id] = True
+                        logging.info("First time on model {} - copying model 0".format(gm_id))
                     local_model = NN_model(dataset, seed)
                     local_model.compile(dataset)
                     local_model.set_weights(global_weights)
@@ -83,7 +96,7 @@ def train_and_average(global_models, dataset, clients_data, timestep, seed):
                 logging.info("Did not average models on timestep {} cround {} of model {}".format(timestep, cround, gm_id))
         logging.info("")
 
-    return global_models
+    return global_models, gm_has_been_trained
 
 def test_models(global_models, clients_data, clients_metrics, dataset, timestep, clients_identities):
     for client_id, (client_data, client_metrics) in enumerate(zip(clients_data[timestep], clients_metrics)):
