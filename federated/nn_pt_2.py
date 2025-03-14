8e2c801e059047624742f5a253c82b38d9d08d43
teresalazar13 <teresa.sal13@gmail.com>
2025-03-14 11:28:37 +0000

    Small test.

--- federated/nn_pt_2.py
+++ federated/nn_pt_2.py
@@ -63,10 +63,10 @@ class NNPTLarge(torch.nn.Module):
         self.dropout = torch.nn.Dropout(0.25)
 
     def forward(self, x):
-        x = self.pool(torch.nn.functional.F.relu(self.bn1(self.conv1(x))))
-        x = self.pool(torch.nn.functional.F.relu(self.bn2(self.conv2(x))))
+        x = self.pool(torch.nn.functional.relu(self.bn1(self.conv1(x))))
+        x = self.pool(torch.nn.functional.relu(self.bn2(self.conv2(x))))
         x = torch.flatten(x, start_dim=1)
-        x = torch.nn.functional.F.relu(self.fc1(x))
+        x = torch.nn.functional.relu(self.fc1(x))
         x = self.dropout(x)
         x = self.fc2(x)
 
