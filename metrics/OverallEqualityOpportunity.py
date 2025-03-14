c94fbd20ef6a8778a8650d2a9ce6bcc624a89e09
teresalazar13 <teresa.sal13@gmail.com>
2025-03-14 14:15:00 +0000

    Small test.

--- metrics/OverallEqualityOpportunity.py
+++ metrics/OverallEqualityOpportunity.py
@@ -41,8 +41,7 @@ class OverallEqualityOpportunity(Metric):
 
             if total_unpriv != 0 and total_priv != 0:
                 res_list.append(res)
-
-            logging.info("{} - {}".format(y, res))
+                logging.info("{} - {}".format(y, res))
 
         return sum(res_list) / len(res_list)
 
