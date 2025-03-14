500052318b99dd26c3ce24f0fb51a4af6ea1d7e4
teresalazar13 <teresa.sal13@gmail.com>
2025-03-14 18:52:07 +0000

    Small test.

--- datasets/image/ImageDataset.py
+++ datasets/image/ImageDataset.py
@@ -6,7 +6,6 @@ from scipy.ndimage import rotate
 import matplotlib.pyplot as plt
 
 
-
 class ImageDataset(Dataset):
 
     def __init__(self, name, input_shape, is_pt, is_binary_target, X, y):
@@ -14,6 +13,7 @@ class ImageDataset(Dataset):
         self.X = X
         self.y = y
 
+    """
     def create_batched_data(self, varying_disc):
         drift_ids = self.drift_ids
         n_clients = self.n_clients
@@ -36,13 +36,6 @@ class ImageDataset(Dataset):
                 X_unpriv_round_client = self.negate(X_priv_round_client)
                 y_unpriv_round_client = y_priv_round_client.copy()
                 if drift_id != 0:
-                    #if self.is_pt:  # if is CIFAR-100 (nothing to do with PyTorch)
-                        #for k in range(3):
-                            #y_unpriv_round_client[y_unpriv_round_client == drift_id + k] = 100
-                            #y_unpriv_round_client[y_unpriv_round_client == drift_id + k + 10] = 101
-                            #y_unpriv_round_client[y_unpriv_round_client == 100] = drift_id + k + 10
-                            #y_unpriv_round_client[y_unpriv_round_client == 101] = drift_id + k
-                    #else:
                     y_unpriv_round_client[y_unpriv_round_client == drift_id] = 100
                     y_unpriv_round_client[y_unpriv_round_client == drift_id + 1] = 101
                     y_unpriv_round_client[y_unpriv_round_client == 100] = drift_id + 1
@@ -61,16 +54,73 @@ class ImageDataset(Dataset):
                 batched_data_round.append([X, y, s, y_original])
             batched_data.append(batched_data_round)
 
+        return batched_data"""
+
+    def create_batched_data(self, varying_disc):
+        drift_ids = self.drift_ids
+        n_clients = self.n_clients
+        n_timesteps = self.n_timesteps
+        X_priv, y_priv = self.get_classes([5, 6, 7, 8, 9], None)
+        X_unpriv, y_unpriv = self.get_classes([0, 1, 2, 3, 4], round(len(X_priv) * varying_disc))
+        X_priv_rounds = np.array_split(X_priv, n_timesteps)
+        y_priv_rounds = np.array_split(y_priv, n_timesteps)
+        X_unpriv_rounds = np.array_split(X_unpriv, n_timesteps)
+        y_unpriv_rounds = np.array_split(y_unpriv, n_timesteps)
+        batched_data = []
+
+        for i in range(n_timesteps):
+            batched_data_round = []
+            X_priv_round_clients = np.array_split(X_priv_rounds[i], n_clients)
+            y_priv_round_clients = np.array_split(y_priv_rounds[i], n_clients)
+            X_unpriv_round_clients = np.array_split(X_unpriv_rounds[i], n_clients)
+            y_unpriv_round_clients = np.array_split(y_unpriv_rounds[i], n_clients)
+            for j in range(n_clients):
+                drift_id = drift_ids[i][j]
+                X_priv_round_client = X_priv_round_clients[j]
+                y_priv_round_client = y_priv_round_clients[j]
+                X_unpriv_round_client = X_unpriv_round_clients[j]
+                y_unpriv_round_client = y_unpriv_round_clients[j]
+                if drift_id != 0:
+                    y_unpriv_round_client[y_unpriv_round_client == drift_id] = 100
+                    y_unpriv_round_client[y_unpriv_round_client == drift_id + 1] = 101
+                    y_unpriv_round_client[y_unpriv_round_client == 100] = drift_id + 1
+                    y_unpriv_round_client[y_unpriv_round_client == 101] = drift_id
+                X = np.concatenate((X_priv_round_client, X_unpriv_round_client), axis=0)
+                y_original = np.concatenate((y_priv_round_client, y_unpriv_round_client), axis=0)
+                s = [1] * len(X_priv_round_client) + [0] * len(X_unpriv_round_client)
+                X = X.astype('float32') / 255.0
+                y = to_categorical(y_original)
+                perm = list(range(0, len(X)))
+                random.shuffle(perm)
+                X = X[perm]
+                y = y[perm]
+                s = np.array(s)[perm]
+                batched_data_round.append([X, y, s, y_original])
+            batched_data.append(batched_data_round)
+
         return batched_data
+
+    def get_classes(self, classes, size):
+        mask = np.isin(self.y, classes)
+        mask = mask[:, np.newaxis]
+        if size is None:
+            X_filtered = self.X[mask.squeeze()]
+            y_filtered = self.y[mask.squeeze()]
+        else:
+            X_filtered = self.X[mask.squeeze()][:size]
+            y_filtered = self.y[mask.squeeze()][:size]
+
+        return X_filtered, y_filtered
 
     def negate(self, X_priv_round_client):
         if not self.is_pt:  # MNIST and FashionMNIST
             return np.rot90(X_priv_round_client.copy() * -1, axes=(-2, -1))
         else:  # CIFAR-100 and CIFAR-10
-            #inverted = X_priv_round_client.copy()
-            #inverted[..., :3] = 255 - inverted[..., :3]
+            # inverted = X_priv_round_client.copy()
+            # inverted[..., :3] = 255 - inverted[..., :3]
             grayscale = X_priv_round_client.copy()
-            grayscale[..., 0] = 0.2989 * X_priv_round_client[..., 0] + 0.5870 * X_priv_round_client[..., 1] + 0.1140 * X_priv_round_client[..., 2]
+            grayscale[..., 0] = 0.2989 * X_priv_round_client[..., 0] + 0.5870 * X_priv_round_client[..., 1] + 0.1140 * \
+                                X_priv_round_client[..., 2]
             grayscale[..., 1] = grayscale[..., 0]
             grayscale[..., 2] = grayscale[..., 0]
             """
