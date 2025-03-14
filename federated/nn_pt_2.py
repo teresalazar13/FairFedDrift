1da68605ab20916db5fe0acaa7f976e1e57f7f21
teresalazar13 <teresa.sal13@gmail.com>
2025-03-14 11:43:01 +0000

    Small test.

--- federated/nn_pt_2.py
+++ federated/nn_pt_2.py
@@ -57,17 +57,23 @@ class NNPTLarge(torch.nn.Module):
         self.bn1 = torch.nn.BatchNorm2d(32)
         self.conv2 = torch.nn.Conv2d(32, 64, kernel_size=3, padding=1)
         self.bn2 = torch.nn.BatchNorm2d(64)
+        self.conv3 = torch.nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.bn3 = torch.nn.BatchNorm2d(128)
         self.pool = torch.nn.MaxPool2d(2, 2)
-        self.fc1 = torch.nn.Linear(64 * 8 * 8, 256)
-        self.fc2 = torch.nn.Linear(256, 10)
-        self.dropout = torch.nn.Dropout(0.25)
+        self.fc1 = torch.nn.Linear(128 * 4 * 4, 512)
+        self.fc2 = torch.nn.Linear(512, 256)
+        self.fc3 = torch.nn.Linear(256, 10)
+        self.dropout = torch.nn.Dropout(0.3)
 
     def forward(self, x):
         x = self.pool(torch.nn.functional.relu(self.bn1(self.conv1(x))))
         x = self.pool(torch.nn.functional.relu(self.bn2(self.conv2(x))))
+        x = self.pool(torch.nn.functional.relu(self.bn3(self.conv3(x))))
         x = torch.flatten(x, start_dim=1)
         x = torch.nn.functional.relu(self.fc1(x))
         x = self.dropout(x)
-        x = self.fc2(x)
+        x = torch.nn.functional.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = self.fc3(x)
 
         return x
